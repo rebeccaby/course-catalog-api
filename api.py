46ca790e92ad16897824d1381eff9d8a67c50963
Rebecca Byun <rebecca_byun@hotmail.com>
2021-04-14 02:10:16 -0500

    Finished GET, POST, DELETE

--- api.py
+++ api.py
@@ -1,31 +1,78 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse, abort
-import requests
+#!usr/bin/env python3
 
-cast = {
-    'id': 0,
-    'name': 'John Wick',
-    'played_by': 'Keanu Reeves'
-}
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-def abort_if_cast_doesnt_exist(cast_id):
-    if cast_id not in cast:
-        abort(404, message=f"ID {cast_id} doesn't exist")
+class CourseModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
-class Cast_Member(Resource):
-    def get(self, cast_id):
-        abort_if_cast_doesnt_exist(cast_id)
-        return cast[cast_id]
+    def __repr__(self):
+        return f'Course name = {self.name}'
 
-class Full_Cast(Resource):
+    def to_json(self):
+        return {
+            'id': int(self.id),
+            'name': self.name
+        }
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str, required=True, help='Name cannot be blank.')
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String
+}
+
+class Course(Resource):
+    # Read
+    @marshal_with(resource_fields)
+    def get(self, course_id): #read
+        result = CourseModel.query.filter_by(id=course_id).first()
+        if not result:
+            abort(409, message="Course id doesn't exist.")
+        return result
+
+    # Create
+    @marshal_with(resource_fields)
+    def post(self, course_id): #create
+        args = parser.parse_args()
+        result = CourseModel.query.filter_by(id=course_id).first()
+        if result:
+            abort(409, message="Course id taken.")
+        course = CourseModel(id=course_id, name=args['name'])
+        db.session.add(course)
+        db.session.commit()
+        return course, 201
+
+    # Update
+    @marshal_with(resource_fields)
+    def put(self, course_id):
+        '''return course, 204'''
+        pass
+
+    # Delete
+    def delete(self, course_id):
+        result = CourseModel.query.filter_by(id=course_id).first()
+        if not result:
+            abort(409, message="Course id doesn't exist.")
+        db.session.delete(result)
+        db.session.commit()
+        return '', 204
+
+class CourseList(Resource):
     def get(self):
-        return cast
+        result = CourseModel.query.all()
+        return [r.to_json() for r in result]
 
-api.add_resource(Full_Cast, '/', '/cast')
-api.add_resource(Cast_Member, '/cast/<cast_id>')
+api.add_resource(CourseList, '/', '/course')
+api.add_resource(Course, '/course/<int:course_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
